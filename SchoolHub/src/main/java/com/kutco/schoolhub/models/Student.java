624bbf05ea3bf385e6aff1ef7df56d612a0734a6
Willem Melis <zilleplus@gmail.com>
2014-10-25 16:55:38 +0200

    add student again after problems

--- SchoolHub/src/main/java/com/kutco/schoolhub/models/Student.java
+++ SchoolHub/src/main/java/com/kutco/schoolhub/models/Student.java
@@ -1,5 +1,30 @@
 package com.kutco.schoolhub.models;
 
 public class Student {
-
+	private int id;
+	private String name;
+	/*
+	 * getter for id
+	 */
+	public int getId(){
+		return id;
+	}
+	/*
+	 * getter for name
+	 */
+	public String getName(){
+		return name;
+	}
+	/*
+	 * setter for id
+	 */
+	public void setId(int id){
+		this.id = id;
+	}
+	/*
+	 * setter for name
+	 */
+	public void setName(String name){
+		this.name =name;
+	}
 }

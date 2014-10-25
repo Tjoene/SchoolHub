9df97fd6773fc73356675332d3863664174cd281
Jeroen Behaegel <jeroenbehaegel@hotmail.com>
2014-10-25 18:17:31 +0200

    Adding a login method to the controller

--- SchoolHub/src/main/java/com/kutco/schoolhub/auth/AuthTest.java
+++ SchoolHub/src/main/java/com/kutco/schoolhub/auth/AuthTest.java
@@ -3,6 +3,7 @@ package com.kutco.schoolhub.auth;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -21,13 +22,24 @@ public class AuthTest {
 
 	@RequestMapping(value = "/admin**", method = RequestMethod.GET)
 	public ModelAndView adminPage() {
-
 		ModelAndView model = new ModelAndView();
 		model.addObject("title", "Spring Security Test");
 		model.addObject("message", "This is protected page - Admin Page!");
 		model.setViewName("admin");
 
 		return model;
+	}
 
+	@RequestMapping(value = "/login", method = RequestMethod.GET)
+	public ModelAndView login(@RequestParam(value = "error", required = false) String error) {
+		ModelAndView model = new ModelAndView();
+		model.addObject("title", "Spring Security Test");
+		model.addObject("message", "This is protected page - Admin Page!");
+		if (error != null) {
+            model.addObject("error", "Invalid username and password!");
+        }
+		model.setViewName("login");
+
+		return model;
 	}
 }

629aa5460c667dfdbd5dd6ccd61548541a481304
Jeroen Behaegel <jeroenbehaegel@hotmail.com>
2014-10-29 09:51:10 +0100

    Adding basic class variables and annotations

--- SchoolHub/src/test/resources/com/kutco/schoolhub/test/AlternativeSecurityTest.java
+++ SchoolHub/src/test/resources/com/kutco/schoolhub/test/AlternativeSecurityTest.java
@@ -1,5 +1,24 @@
 package com.kutco.schoolhub.test;
 
-public class AlternativeSecurityTest {
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.web.FilterChainProxy;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.web.context.WebApplicationContext;
 
+@RunWith(SpringJUnit4ClassRunner.class)
+@WebAppConfiguration
+@ContextConfiguration({ "file:src/main/webapp/WEB-INF/spring/appServlet/security-context.xml",
+        "file:src/main/webapp/WEB-INF/spring/appServlet/servlet-context.xml" })
+public class AlternativeSecurityTest {
+    @Autowired
+    private WebApplicationContext wac; // Interface to provide configuration for a web application
+
+    @Autowired
+    private FilterChainProxy springSecurityFilter;
+
+    private MockMvc mockMvc; // Main entry point for server-side Spring MVC test
 }

46a18b7c2143a26337e2d171260655c3b373eef5
Jeroen Behaegel <jeroenbehaegel@hotmail.com>
2014-10-29 10:05:15 +0100

    Add method to test is an unauthorized person gets booted from pages he isn't allowed to visit.

--- SchoolHub/src/test/resources/com/kutco/schoolhub/test/AlternativeSecurityTest.java
+++ SchoolHub/src/test/resources/com/kutco/schoolhub/test/AlternativeSecurityTest.java
@@ -3,13 +3,20 @@ package com.kutco.schoolhub.test;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrl;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppContextSetup;
 
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mock.web.MockHttpSession;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.FilterChainProxy;
+import org.springframework.security.web.context.HttpSessionSecurityContextRepository;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
@@ -56,5 +63,39 @@ public class AlternativeSecurityTest {
     @Test
     public void askLogin() throws Exception {
         this.mockMvc.perform(get(this.securedPage)).andExpect(redirectedUrl(this.loginURL));
+    }
+
+    /**
+     * This method will test if an user can access the secured page that he has access to.
+     * 
+     * @throws Exception
+     */
+    @Test
+    public void allowAccess() throws Exception {
+        Authentication authentication = new UsernamePasswordAuthenticationToken("admin", "password");
+        SecurityContext securityContext = SecurityContextHolder.getContext();
+        securityContext.setAuthentication(authentication);
+
+        MockHttpSession session = new MockHttpSession();
+        session.setAttribute(HttpSessionSecurityContextRepository.SPRING_SECURITY_CONTEXT_KEY, securityContext);
+
+        this.mockMvc.perform(get(this.securedPage).session(session)).andExpect(status().isOk());
+    }
+
+    /**
+     * This method will test if an unauthorized person gets kicked of the secured page.
+     * 
+     * @throws Exception
+     */
+    @Test
+    public void denyAccess() throws Exception {
+        Authentication authentication = new UsernamePasswordAuthenticationToken("tjoene", "password");
+        SecurityContext securityContext = SecurityContextHolder.getContext();
+        securityContext.setAuthentication(authentication);
+
+        MockHttpSession session = new MockHttpSession();
+        session.setAttribute(HttpSessionSecurityContextRepository.SPRING_SECURITY_CONTEXT_KEY, securityContext);
+
+        this.mockMvc.perform(get(this.securedPage).session(session)).andExpect(status().isForbidden());
     }
 }
